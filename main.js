9d044e75d4eaa2c8fb3bdb93c5bf45bdb71d3b45
Andrii Danyliuk <andriidanyliuk1@gmail.com>
2022-04-29 10:56:48 +0300

    Перевірка в LocalStorage.

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 // button з ToDoList 
 const addTaskBtn = document.getElementById('add-task-btn');
-// input з ToDoList
+// значення input з ToDoList
 const deskTaskInput = document.getElementById('description-task');
 // <div class="todos-wrapper"> з "Завдання на день:"
 const todoWrapper = document.querySelector('.todos-wrapper');
 
 // створюємо пустий масив (для зберігання завдань) 
-let tasks = [];
+let tasks;
+// Перевіримо, чи є в LocalStorage в масиві tasks якісь об'єкти; якщо в LocalStorage нічого немає, то створимо пустий масив, якщо щось є, то отримаємо значення
+!localStorage.tasks ? tasks = [] : JSON.parse(localStorage.getItem('tasks'));
 
 // Створюю конструктор, для формування списку завдань, які будуть зберігатися в F12-Application-LocalStorage-file(http)
 function Task(description) {
-    // description будемо передавати
+    // description будемо передавати при створенні завдання (з input)
     this.description = description;
     // по замовчуванню виконання завдання (completed)
     this.completed = false;
@@ -18,7 +20,7 @@ function Task(description) {
 
 // Функція для оновлення Local Storage
 const updateLocal = () => {
-    // localStorage - глобальна назва (яд document); setItem(key: string, value: string); JSON.stringify(tasks) - перетворює масив tasks в JSON-формат (так краще для Local Storage) 
+    // localStorage - глобальна назва (як document); setItem(key: string, value: string) - встановити значення; JSON.stringify(tasks) - перетворює масив tasks в JSON-формат (так краще для Local Storage) 
     localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
